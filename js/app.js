90487a68ca2c01a37f08e8503e85bd76c8fefaef
LavieMaru <laviemaru@gmail.com>
2016-08-29 15:58:53 -0400

    callback function

--- js/app.js
+++ js/app.js
@@ -4,34 +4,42 @@
 // //part: 'snippet'
 // key: (your API key as a string)
 // q: (your search term as a string)
-
-$(function(){
-	$("#search-term").submit(function(event){
-		event.preventDefault(); 
-		var search = $("#query").val();
-		getRequest(searchTerm);
-	})
+// var youtube-link = 
 
 
+
+$(function() {
+    $("#search-term").submit(function(event) {
+        event.preventDefault();
+        var searchTerm = $("#query").val();
+        $('#query').val(''); //calling it
+        getRequest(searchTerm);
+    });
 });
 
-function getRequest(searchTerm){
-	var param = {
-		part:'snippet', 
-		key: 'AIzaSyB0C85q-yECJMIwnvv39oSP24Qvw5ITjNY', 
-		q: 'searchTerm' 
-	}
+function getRequest(searchTerm) {
+    var params = {
+        part: 'snippet',
+        key: 'AIzaSyB0C85q-yECJMIwnvv39oSP24Qvw5ITjNY',
+        q: searchTerm, 
 
-url= 'https://www.googleapis.com/youtube/v3/search';
-$.getJSON(url, param, function(data){
-    showResults(data.Search);
-  });
+    }
+
+    $.getJSON('https://www.googleapis.com/youtube/v3/search', params, function(data) {
+     var showResults = data.items; //why is data.items used??? how do i know if it's items, and not other key? which website do i use as a reference for this key in devtools? 
+      console.log(data);
+       // $.each(searchResult, function(index, value) {
+       // 		//assign variable to youtube url to be assigned to youtube ID
+       // 		var youtubeUrl = "https://www.youtube.com/watch?v=" + item.id.videoId;
+       // 		//assign variable to picture of the video snippet 
+
+
+       	
+       // });
+    });
 
 };
 
-function showResults (results) {
-	var HTML = "";
-	$.each(results, function(index, value) {
-		html += '<p>' + value.Title + '</p>';
-		console.log(value.Title)
-}
+// function showResults(picResults, linkResults) {
+//    $('.result')
+//     };
